3eb358dd488488f20cd61fae692bdca0f123f727
Viktor Gofman <vsgof@yandex.ru>
2023-03-19 21:34:04 +0300

    feat: роуты для логина и регистрации

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const { createUser, login } = require('./controllers/users');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -10,6 +11,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/signin', login);
+app.post('/signup', createUser);
 app.use((req, res, next) => {
   req.user = {
     _id: '640d92eff3a9a6b34146f223',
@@ -18,6 +21,7 @@ app.use((req, res, next) => {
 });
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Страница не найдена.' });
 });
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,11 @@ const router = require('express').Router();
 const {
   getUsers,
   getUserById,
-  createUser,
   updateProfile,
   updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.post('/', createUser);
 router.get('/:userId', getUserById);
 router.patch('/me', updateProfile);
 router.patch('/me/avatar', updateAvatar);
