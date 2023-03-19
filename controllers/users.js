06f8e08e66848fc944c9166240f23a43ec1a64ef
Viktor Gofman <vsgof@yandex.ru>
2023-03-19 19:50:24 +0300

    feat: добавить в createUser хеш пароля и изм. аргументы

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 const getUsers = (req, res) => User.find({})
@@ -20,9 +21,13 @@ const getUserById = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => res.status(201).send(user))
+  const {
+    email, password, name, about, avatar,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      email, password: hash, name, about, avatar,
+    })).then((user) => res.status(201).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
--- models/user.js
+++ models/user.js
@@ -17,20 +17,20 @@ const userSchema = new mongoose.Schema(
     },
     name: {
       type: String,
-      required: true,
       minlength: 2,
       maxlength: 30,
+      default: 'Жак-Ив Кусто',
     },
     about: {
       type: String,
-      required: true,
       minlength: 2,
       maxlength: 30,
+      default: 'Исследователь',
     },
     avatar: {
       type: String,
-      required: true,
       validate: { validator: validateUrl, message: 'Введите валидную ссылку' },
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     },
   },
   {
