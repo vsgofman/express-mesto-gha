9fc164e2be5bc7f9d716b442b1bf454dab0d156c
Viktor Gofman <vsgof@yandex.ru>
2023-03-19 13:36:46 +0300

    feat: email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const { validateUrl } = require('../utils/validateUrl');
 
 const userSchema = new mongoose.Schema(
   {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: { validator: validator.isEmail, message: 'Введите корректный email' },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
     name: {
       type: String,
       required: true,
@@ -25,5 +37,18 @@ const userSchema = new mongoose.Schema(
     versionKey: false,
   },
 );
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  // попытаемся найти пользователя по почте
+  return this.findOne({ email }) // this — это модель User
+    .then((user) => {
+      // не нашёлся — отклоняем промис
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      // нашёлся — сравниваем хеши
+      return bcrypt.compare(password, user.password);
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
