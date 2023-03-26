d48ad29f7b89a915109cf0cb82b9cf266a2498a6
Viktor Gofman <vsgof@yandex.ru>
2023-03-26 14:02:02 +0300

    fix: исправить ошибки после тестов

--- utils/validateUrl.js
+++ utils/validateUrl.js
@@ -1,6 +1,6 @@
 function validateUrl(url) {
-  const regex = /^https?:\/\/(www\.)?[a-zA-Z\d]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/g;
-  if (regex.test(url)) {
+  const RegExp = /^https?:\/\/(www\.)?[a-zA-Z\d]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/g;
+  if (RegExp.test(url)) {
     return url;
   }
   throw new Error();
